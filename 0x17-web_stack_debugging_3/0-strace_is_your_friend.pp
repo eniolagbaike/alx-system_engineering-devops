5b77eeaac92b265a578d6133766982d113f8409e
eniolagbaike <eniolagbaike@gmail.com>
2023-10-11 13:26:21 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# Fix 500 error when a GET HTTP method is requested to Apache web server
-
-exec {'replace':
-  provider => shell,
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-}
+# puppet code to fix WordPress webstack bug
+exec { 'debugging fix':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/bin'
+  }
